f746133f94a4fb903d78f744b8e2593d72a78d27
mtkfork <yudiparth@gmail.com>
2017-06-06 12:13:34 +0530

    aosp bringup

--- aosp_m2.mk
+++ aosp_m2.mk
@@ -4,12 +4,9 @@ PRODUCT_RELEASE_NAME := m2
 # Inherit Complete Device Configuration
 $(call inherit-product, device/gionee/m2/full_m2.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m2
-PRODUCT_NAME := lineage_m2
+PRODUCT_NAME := aosp_m2
 PRODUCT_BRAND := gionee
 PRODUCT_MODEL := m2
 PRODUCT_MANUFACTURER := gionee 
